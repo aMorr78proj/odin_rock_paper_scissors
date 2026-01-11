e4d4a9795f3384fdb28171a5200a4aeee22eee00
aMorr78proj <234978669+aMorr78proj@users.noreply.github.com>
2026-01-11 11:00:13 -0800

    Finished adding buttons, styling, and adjusting game to DOM events

--- javascript/main.js
+++ javascript/main.js
@@ -23,24 +23,25 @@ function computerInput() {
 
 function winComparison(user, computer) {
     let win = "";
+    let comment = "";
 
     if (user === computer) 
     {
-        console.log("It's a tie! Try again!");
+        comment = "It's a tie! Try again!";
         win = "none";
     } 
     else if (  (user === "rock"     && computer === "scissors")
             || (user === "paper"    && computer === "rock")
             || (user === "scissors" && computer === "paper"))
     {
-        console.log("You win!");
+        comment = "You win!";
         win = "user";
     }
     else if (  (computer === "rock"     && user === "scissors")
             || (computer === "paper"    && user === "rock")
             || (computer === "scissors" && user === "paper"))
     {
-        console.log("Computer wins!");
+        comment = "Computer wins!";
         win = "computer";
     }
     else 
@@ -48,28 +49,57 @@ function winComparison(user, computer) {
         win = "none";
     }
 
-    return win;
+    return [win, comment];
 }
 
 function score(lastWin) {
-    console.log(lastWin);
+    let scoreAddition = 0;
+
     if (lastWin === "user") {
         userScore+=1;
-        console.log(userScore);
+        scoreAddition = userScore;
     }else if (lastWin === "computer") {
         computerScore+=1;
-        console.log(computerScore);
+        scoreAddition = computerScore;
     }
 
-    console.log(`The score is now ${userScore} to ${computerScore}!`);
+    let score = document.querySelector(`.${lastWin}-score`);
+    score.textContent = scoreAddition;
+
+
+    // console.log(`The score is now ${userScore} to ${computerScore}!`);
 }
 
 function playRound(user) {
 
+    if (user === "reset")
+    {
+        userScore = 0;
+        computerScore = 0;
+
+        let userScoreDisplay = document.querySelector(".user-score");
+        let computerScoreDisplay = document.querySelector(".computer-score");
+        let commentary = document.querySelector(".commentary");
+
+        userScoreDisplay.textContent = 0;
+        computerScoreDisplay.textContent = 0;
+        commentary.textContent = null;
+    }
+    else 
+    {
         // let user = userInput();
         let computer = computerInput();
-        console.log(`You selected ${user}. The computer selected ${computer}`);
-        let winner = winComparison(user, computer);
-        score(winner);
+
+        let commentary = document.querySelector(".commentary");
+        commentary.textContent = `You selected ${user}. The computer selected ${computer}. \n`;
+        // console.log(`You selected ${user}. The computer selected ${computer}`);
+        
+        let results = winComparison(user, computer);
+        let winner = results[0];
+        let extraComment = results[1];
+
+        commentary.textContent += `${extraComment}`;
+        if (winner != "none") { score(winner); }
+    }
 }
 
